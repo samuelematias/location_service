ac4035efd59008c4ace00367f417337d7a0e35be
samuelematias <hi@samuelematias.com>
2020-08-10 13:21:21 -0300

    feat(views): create HomeViews

---
+++ lib/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:location_service/datamodels/user_location.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var userLocation = Provider.of<UserLocation>(context);
+    return Center(
+      child: Text(
+          'Location: Lat${userLocation?.latitude}, Long: ${userLocation?.longitude}'),
+    );
+  }
+}
